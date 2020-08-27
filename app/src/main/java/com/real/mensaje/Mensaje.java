4f32b715ffd9ea050b6a39b6e398f261751d1a66
The Real <adrianreal2001@gmail.com>
2020-08-27 01:02:52 -0400

    practica 20

---
+++ app/src/main/java/com/real/mensaje/Mensaje.java
@@ -0,0 +1,14 @@
+package com.real.mensaje;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Mensaje extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mensaje);
+    }
+}
